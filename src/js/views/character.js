e5cea714a0f251b5f07bee4dd8cdff59dbab8d90
Bethania Sanchez <bethasanchez3@gmail.com>
2021-11-30 22:29:02 -0400

    Cambios previos

--- src/js/views/character.js
+++ src/js/views/character.js
@@ -34,6 +34,9 @@ export const Character = item => {
 							This is a wider card with supporting text below as a natural lead-in to additional content.
 							This content is a little bit longer.
 						</p>
+						<Link to={`/`} type="button" className="btn btn-warning homebutton">
+							Go back home!
+						</Link>
 					</div>
 				</div>
 			</div>
